7461c113c6aed44afd3c37d16173ef92a03f8f1f
aniganesh <aniganesh741@gmail.com>
2020-11-01 20:48:11 +0530

    Dashboard styles fixed, bootstrap resets added

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,31 +1,38 @@
-const dashboard = (options)=>{
+const dashboard = (options) => {
     const dashboard = div()
-    options.forEach((option)=>{
-        dashboard.appendChild(createCard(option.content,option.title))
-        
+    dashboard.classList.add('full-height-flex')
+    options.forEach((option) => {
+        const { title, content } = option
+        const link = document.createElement('a')
+        link.classList.add('dashboard-item')
+        link.style['display'] = 'inline-block'
+        link.href = option.url
+        link.appendChild(createCard(content, title))
+        dashboard.appendChild(link)
+
     })
     return dashboard
 
 }
-const createCard = (content,title)=>{
- const card = div()
- card.classList.add('card')
- const cardTitle = div()
- cardTitle.innerText=title
- const cardContent = div()
- cardContent.innerText=content
- const cardBody = div()
- cardBody.classList.add('card-body')
- card.appendChild(cardBody)
- cardBody.appendChild(cardTitle)
- cardBody.appendChild(cardContent)
- return card
-} 
-const div = ()=>{
+const createCard = (content, title) => {
+    const card = div()
+    card.classList.add('card')
+    const cardTitle = div()
+    cardTitle.innerText = title
+    const cardContent = div()
+    cardContent.innerText = content
+    const cardBody = div()
+    cardBody.classList.add('card-body')
+    card.appendChild(cardBody)
+    cardBody.appendChild(cardTitle)
+    cardBody.appendChild(cardContent)
+    return card
+}
+const div = () => {
     return document.createElement('div')
 }
 const root = document.querySelector('#root')
 
-const options = [{title:'Add stock',content:``,url:`addStock`},{title:`Update stock`,content:` `,url:`updateStock`},{title:`Tag stock`,content:``,url:`tagStock`},{title:`Allot space`,content:``,url:`allotSpace`},
-{title:`View all stocks`,content:``,url:`viewAllStocks`}]
+const options = [{ title: 'Add stock', content: ``, url: `addStock` }, { title: `Update stock`, content: ` `, url: `updateStock` }, { title: `Tag stock`, content: ``, url: `tagStock` }, { title: `Allot space`, content: ``, url: `allotSpace` },
+{ title: `View all stocks`, content: ``, url: `viewAllStocks` }]
 root.appendChild(dashboard(options))
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 const token = sessionStorage.getItem('access-token')
 
-console.log({ token })
+const getHeader = (title) => {
+	const header = createDiv();
+	header.classList.add('navbar')
+	header.classList.add('theme-primary-bg')
+	const titleComp = document.createElement('div')
+	titleComp.classList.add('h4')
+	titleComp.innerText = title
+	header.appendChild(titleComp)
+	return header
+}
+
+const createDiv = () => {
+	return document.createElement('div')
+}
